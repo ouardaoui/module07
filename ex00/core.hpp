543940e16bd158c534835d864adaad443d6fd4a0
ouardaoui <ayoubouardaoui3@gmail.com>
2023-09-21 17:25:55 +0100

    ex00

---
+++ ex00/core.hpp
@@ -0,0 +1,33 @@
+#ifndef  CORE_H
+#define CORE_H
+
+#include<iostream>
+
+template <typename T>
+
+void swap(T  &a,T &b)
+{
+  T tmp;
+
+  tmp = a;
+  a = b;
+  b = tmp;
+}
+
+
+template <typename T>
+T min(T &a, T &b)
+{
+  
+  return (a < b  ?  a: b);
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+  
+  return (a > b  ?  a: b);
+}
+
+
+#endif // ! CORE_H
